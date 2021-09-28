b65434ce82ceec514b6ef50064a9b8e4abcec881
Nick Sonneveld <nick@sonneveld.name>
2021-09-28 23:08:39 +1000

    add create_english script

---
+++ create_english.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import re
+import csv
+import shutil
+import os
+
+'''
+Take the translated text and produce an english release.
+'''
+
+
+with open("deutsch/FROSCH.EXE", "rb") as f:
+    data = f.read()
+data = bytearray(data)
+
+
+with open("english.csv", "r") as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+    for row in spamreader:
+
+        addr = int(row[0])
+        strlen = int(row[1])
+        english = row[3]
+
+        exe_addr = addr + 0xf70
+
+        if english == "TODO":
+            continue
+    
+        # print (addr, strlen, english)
+        if len(english) > strlen:
+            print(f"str '{english}' is too long. Will only see '{english[:strlen]}'")
+
+        strdata = english.encode("cp437") + b" "*100
+        strdata = strdata[:strlen]
+        # print(repr(strdata))
+
+        data[exe_addr+1:exe_addr+1+strlen] = strdata
+
+
+os.makedirs("english")
+
+with open("english/FROGS.EXE", 'wb') as f:
+    f.write(data)
+
+files = [
+    'deutsch/FROG.DRV',
+    'ENGFROG.INF',
+    'file_id.diz',
+]
+
+for fname in files:
+    shutil.copy(fname, "english/")
